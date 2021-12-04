d29f7377a618c50716e833b19641f6f11a1ae2c9
tullioColacicco <56273725+tullioColacicco@users.noreply.github.com>
2021-12-03 19:17:10 -0500

    nes css grid and cards template

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import "nes.css/css/nes.min.css";
+import { FC } from 'react';
+import Cards from './components/cards';
+
+const App: FC =() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="parent">
+  <div id="pageHeader">
+  <span className="nes-text is-primary">Primary</span>
+  <div>
+    
+  <i className="nes-ash"></i>
+  <i className="nes-pokeball"></i>
+  <i className="nes-bulbasaur"></i>
+  <i className="nes-charmander"></i>
+  <i className="nes-squirtle"></i>
+  <i className="snes-logo"></i>
+  </div>
+  </div>
+  <article id="mainArticle">
+    <div id='container' className="nes-container is-centered is-dark with-title">
+  <p className="title">Container.is-dark</p>
+ <Cards/>
+ 
+</div></article>
+  <nav id="mainNav">Nav</nav>
+  <div id="siteAds">Ads</div>
+  <footer id="pageFooter">Footer</footer>    </div>
   );
 }
 
---
+++ src/components/cards.tsx
@@ -0,0 +1,87 @@
+import { FC } from "react";
+
+const Cards: FC = () => {
+    return (
+
+        <main className="grid">
+  
+        <article className="card">
+          <img src="https://img.pokemondb.net/sprites/silver/shiny/typhlosion.png"alt="Typhlosion"/>
+          <div className="text">
+            <h3>You Got!</h3>
+            <p>Typhlosion</p>
+            <button>More Info</button>
+          </div>
+        </article>
+      
+        <article className="card">
+        <img src="https://img.pokemondb.net/sprites/silver/normal/snorlax.png" alt="Snorlax"/>
+          <div className="text">
+            <h3>You Got!</h3>
+            <p>Typhlosion</p>
+            <button>More Info</button>
+          </div>
+        </article>  
+        
+        <article className="card">
+        <img src="https://img.pokemondb.net/sprites/black-white/anim/shiny/snorlax.gif" alt="Snorlax"/>
+          <div className="text">
+            <h3>You Got!</h3>
+            <p>Typhlosion</p>
+            <button>More Info</button>
+          </div>
+        </article>  
+      
+          <article className="card">
+          <img src="https://img.pokemondb.net/sprites/sword-shield/normal/mimikyu.png" alt="Mimikyu"/>
+            <div className="text">
+              <h3>You Got!</h3>
+              <p>Typhlosion</p>
+              <button>More Info</button>
+            </div>
+          </article>  
+      
+          <article className="card">
+        <img src="https://img.pokemondb.net/sprites/sun-moon/shiny/mimikyu.png" alt="Mimikyu"/>
+            <div className="text">
+              <h3>You Got!</h3>
+              <p>Typhlosion</p>
+              <button>More Info</button>
+            </div>
+          </article>  
+      
+          <article className="card">
+        <img src="https://img.pokemondb.net/sprites/silver/normal/gengar.png" alt="Gengar"/>
+            <div className="text">
+              <h3>You Got!</h3>
+              <p>Typhlosion</p>
+              <button>More Info</button>
+            </div>
+          </article>  
+      
+          <article className="card">
+          <img src="https://img.pokemondb.net/sprites/black-white/anim/normal/venusaur.gif" alt="Venusaur"/>
+            <div className="text">
+              <h3>You Got!</h3>
+              <p>Typhlosion</p>
+              <button>More Info</button>
+            </div>
+          </article>  
+      
+          <article className="card">
+         <img src="https://img.pokemondb.net/sprites/yellow/normal/arcanine-color.png" alt="Arcanine"/>
+            <div className="text">
+              <h3>You Got!</h3>
+              <p>Typhlosion</p>
+              <button>More Info</button>
+            </div>
+          </article>  
+       
+      </main>
+
+
+    )
+    
+}
+
+export default Cards
